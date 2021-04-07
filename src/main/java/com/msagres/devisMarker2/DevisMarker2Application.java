24f562ee490c662b0c87a674e0916cf23f37267b
m_costa3@etu-webschoolfactory.fr <PTg]m3vH6prx7QNGU2}823]}%yd2>
2021-04-07 20:52:51 +0200

    init

---
+++ src/main/java/com/msagres/devisMarker2/DevisMarker2Application.java
@@ -0,0 +1,13 @@
+package com.msagres.devisMarker2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevisMarker2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevisMarker2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/msagres/devisMarker2/DevisMarker2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.msagres.devisMarker2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevisMarker2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
